a274e052cd7ea7a9058b105875de570c6c3c50bb
Stéphane Klein <sklein@bearstech.com>
2013-03-05 10:07:44 +0100

    Improve network configure, now use dnsmasq

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,6 @@ from fabric.api import task, run, env, put
 from fabric.contrib.files import append as append_to_file
 from fabric.contrib.files import sed
 from fabtools.vagrant import ssh_config, _settings_dict
-from fabric.utils import puts
 import fabtools  # NOQA
 
 
@@ -51,7 +50,7 @@ def install():
     fabtools.deb.update_index()
     fabtools.deb.upgrade()
     fabtools.require.deb.packages([
-        'lxc', 'debootstrap', 'bridge-utils', 'libvirt-bin'
+        'lxc', 'debootstrap', 'bridge-utils', 'libvirt-bin', 'dnsmasq'
     ])
     append_to_file(
         '/etc/fstab',
@@ -63,29 +62,40 @@ def install():
         '/usr/lib/lxc/templates/'
     )
     run('chmod ugo+x /usr/lib/lxc/templates/lxc-squeeze-custom')
-    sed(
-        '/etc/network/interfaces',
-        '#VAGRANT-END',
-        """     up iptables -t nat -F POSTROUTING\\n     up iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE\\n\\n#VAGRANT-END"""
-    )
     append_to_file(
         '/etc/network/interfaces',
         """\
+
 auto br0
 iface br0 inet static
-   address 192.168.33.11
+   address 192.168.10.254
    netmask 255.255.255.0
-   bridge_ports eth1
-   bridge_stp off
+   bridge_ports none
+   bridge_stp on
+   bridge_fd 1
    bridge_maxwait 0
-   post-up /usr/sbin/brctl setfd br0 0
+   up iptables -t nat -F POSTROUTING
+   up iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 """
     )
 
     run('sysctl net.ipv4.ip_forward=1')
-    run('iptables -t nat -F POSTROUTING')
-    run('iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE')
+    append_to_file(
+        '/etc/sysctl.conf',
+        'net.ipv4.ip_forward=1'
+    )
     run('ifup br0')
+
+    append_to_file(
+        '/etc/dnsmasq.conf',
+        """\
+interface=br0
+dhcp-range=192.168.10.1,192.168.10.250,12h
+dhcp-option=3,192.168.10.254
+"""
+    )
+    run('echo "nameserver 8.8.8.8" > /etc/resolv.conf')
+    run('/etc/init.d/dnsmasq restart')
 
 
 @task
