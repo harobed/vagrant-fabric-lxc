c16da35a014e7f172d0ad5b2c780ce667304dc21
Olivier Kautz <olivier@k3z.fr>
2014-10-09 13:50:06 +0200

    Updated fabric script

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,13 @@
 import textwrap
-from fabric.api import task, local, run, env, put, settings, hide
+from fabric.api import task, local, run, env, put, settings, hide, shell_env
 from fabric.contrib.files import append as append_to_file
+from fabric.contrib.files import sed
+from fabric.contrib.project import upload_project
 import fabtools  # NOQA
+
+lxc_containers = {
+    'samplebox': '10.0.0.10',
+}
 
 
 def _settings_dict(config):
@@ -21,6 +27,12 @@ def _settings_dict(config):
     return _settings
 
 
+def set_timezone(timezone):
+    fabtools.utils.run_as_root('echo "%s" > /etc/timezone' % timezone)
+    fabtools.utils.run_as_root('dpkg-reconfigure --frontend noninteractive tzdata')
+    fabtools.require.service.restarted('cron')
+
+
 def ssh_config(name=''):
     """
     Get the SSH parameters for connecting to a vagrant VM.
@@ -36,6 +48,12 @@ def ssh_config(name=''):
 
 
 @task
+def remote():
+    env['hosts'] = ['www.hosting.tld']
+    env['user'] = 'root'
+
+
+@task
 def vagrant(name=''):
     config = ssh_config(name)
     extra_args = _settings_dict(config)
@@ -44,30 +62,46 @@ def vagrant(name=''):
 
 
 @task
-def install():
+def update_lxc_template():
+    put(
+        'assets/lxc/lxc-debian-sk',
+        '/usr/share/lxc/templates/'
+    )
+    run('chmod ugo+x /usr/share/lxc/templates/lxc-debian-sk')
+
+
+@task
+def requirements():
     fabtools.deb.update_index()
     fabtools.deb.upgrade()
+    fabtools.require.deb.nopackages(['bind9', 'bind9-host'])
     fabtools.require.deb.packages([
-        'lxc', 'debootstrap', 'bridge-utils', 'libvirt-bin', 'dnsmasq', 'rsync'
+        'lxc', 'debootstrap', 'bridge-utils', 'libvirt-bin',
+        'dnsmasq', 'rsync', 'fail2ban', 'vim', 'mc', 'ntpdate',
+        'fail2ban'
     ])
+    set_timezone('Europe/Paris')
+    run('ntpdate-debian')
+
+
+@task
+def install():
+    requirements()
+
     append_to_file(
         '/etc/fstab',
         'cgroup /sys/fs/cgroup cgroup defaults 0 0'
     )
     run('mount /sys/fs/cgroup')
 
-    put(
-        'templates/lxc-debian-sk',
-        '/usr/share/lxc/templates/'
-    )
-    run('chmod ugo+x /usr/share/lxc/templates/lxc-debian-sk')
+    update_lxc_template()
 
     append_to_file(
         '/etc/network/interfaces',
         textwrap.dedent("""
         auto br0
         iface br0 inet static
-        address 192.168.10.254
+        address 10.0.0.254
         netmask 255.255.255.0
         bridge_ports none
         bridge_stp off
@@ -89,20 +123,47 @@ def install():
         '/etc/dnsmasq.conf',
         textwrap.dedent("""
         interface=br0
-        dhcp-range=192.168.10.1,192.168.10.250,12h
-        dhcp-option=3,192.168.10.254
+        dhcp-range=10.0.0.1,10.0.0.250,12h
+        dhcp-option=3,10.0.0.254
         """)
     )
     run('echo "nameserver 8.8.8.8" > /etc/resolv.conf')
     run('/etc/init.d/dnsmasq restart')
 
-
-@task
-def create_container(name='container1'):
-    run('lxc-create -n %s -t debian-sk' % name)
-    run('lxc-start -n %s -d' % name)
+    install_shorewall()
+    install_pound()
 
 
 @task
-def stop_container(name='container1'):
-    run('lxc-stop -n %s' % name)
+def install_pound():
+    fabtools.require.deb.package('pound')
+    with settings(hide('running'), shell_env()):
+        sed('/etc/default/pound', 'startup=0', 'startup=1')
+
+    put(
+        'assets/pound/pound.cfg',
+        '/etc/pound/pound.cfg'
+    )
+
+    run('service pound start')
+
+
+@task
+def install_shorewall():
+    fabtools.require.deb.package('shorewall')
+    upload_project('assets/shorewall/', '/etc/')
+
+    with settings(hide('running'), shell_env()):
+        sed('/etc/default/shorewall', 'startup=0', 'startup=1')
+
+    run('shorewall start')
+
+
+@task
+def create_all_container():
+    for hostname, ip in lxc_containers.items():
+        run(
+            'lxc-create -n %s-lxc -t debian-sk -- --ip=%s --gateway=10.0.0.254' %
+            (hostname, ip)
+        )
+        run('lxc-start -n %s-lxc -d' % hostname, pty=False)
